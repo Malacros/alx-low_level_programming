d4b4a7af2afd1a7bf0af8e9dda508daa66f81f54
Malacros <okekemalachy@gmail.com>
2022-10-14 20:51:50 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints all digit possible numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int h;
+
+	for (h = 48; h < 58; h++)
+	{
+		putchar(h);
+		if  (h != 57)
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
