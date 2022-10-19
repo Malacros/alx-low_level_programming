65eb6ff7a1b03c907fe73a258269566c7235681b
Malacros <okekemalachy@gmail.com>
2022-10-19 13:14:19 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print abc.
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z', c++)
+
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
