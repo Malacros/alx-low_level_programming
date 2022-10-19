9e029870121f014c3ae5a8987b6a54977c193467
Malacros <okekemalachy@gmail.com>
2022-10-19 14:10:26 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -> prints the lowercase alphabets
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int c;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
