f9f15e646c14dda6a48f2d6237d70895767a1159
Malacros <okekemalachy@gmail.com>
2022-10-24 13:38:07 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _puts - prints a string
+ * @str : A pointer to an int that will be changed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0' ; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
