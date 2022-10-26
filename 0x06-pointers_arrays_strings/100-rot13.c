bd5c90a29621498660afba25b993b4175a6ee885
Malacros <okekemalachy@gmail.com>
2022-10-26 15:56:44 -0700

    task adv 1

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: the string to be encoded
+ * Return: A pointer
+ */
+char *rot13(char *str)
+{
+	int ix1 = 0, ix2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'a', 'b', 'c', 'd',
+		'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p',
+		'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E',
+		'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c',
+		'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm'};
+	while (str[ix1])
+	{
+		for (ix2 = 0; ix2 < 52; ix2++)
+		{
+			if (str[ix1] == alphabet[ix2])
+			{
+				str[ix1] = rot13key[ix2];
+				break;
+			}
+		}
+		ix1++;
+	}
+	return (str);
+}
